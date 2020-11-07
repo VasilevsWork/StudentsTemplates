eab2d219aa75d1253907f29b5a9e6580b0e20cbb
Vasiliy Smagin <47122530+VasilevsWork@users.noreply.github.com>
2020-11-07 03:04:50 +0300

    [example] Добавление примера с обобщенными (Generic) классами и методами.

---
+++ GenericClasses/GenericClasses/GenericMethods.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClasses
+{
+
+    class MyInt : IEquatable<Int32>
+    {
+        Int32 value;
+
+        public MyInt()
+        {
+
+        }
+
+        public MyInt(Int32 val)
+        {
+            value = val;
+        }
+
+        public bool Equals(Int32 other)
+        {
+            return value.Equals(other);
+        }
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+    }
+
+    struct MyDouble : IEquatable<Double>
+    {
+        Double value;
+
+        public MyDouble(Double val)
+        {
+            value = val;
+        }
+
+        public bool Equals(Double other)
+        {
+            return value.Equals(other);
+        }
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+    }
+
+    class GenericExamples 
+    {
+        public static void Swap<T>(ref T x, ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void SwapStruct<T>(ref T x, ref T y) where T : struct
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void SwapDoubles<T>(ref T x, ref T y) where T : IEquatable<Double>
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+    }
+}
---
+++ GenericClasses/GenericClasses/NoGenericAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClasses
+{
+    class GenericAccount<T>
+    {
+        public T Id { get; set; }
+        public int Sum { get; set; }
+    }
+
+    class NoGenericAccount
+    {
+        public object Id { get; set; }
+        public int Sum { get; set; }
+    }
+}
