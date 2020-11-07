6de4e46ef6c5ddf8ab283327c91e7b8efc8cbcf9
Vasiliy Smagin <47122530+VasilevsWork@users.noreply.github.com>
2020-11-07 03:08:23 +0300

    [template] Добавлен пример реализации класса FiguresContainer.

---
+++ FiguresContainer/FiguresContainer/FiguresContainer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace FiguresContainer
+{
+    class Figure
+    {
+        public int counter;
+    }
+    class FiguresContainer : IEnumerable<Figure>
+    {
+        List<Figure> figures = new List<Figure>();
+
+        public void ToXml(string filename)
+        {
+            XmlWriter writer = XmlWriter.Create(filename);
+        }
+        public void FromXml(string filename)
+        {
+            XmlReader reader = XmlReader.Create(filename);
+        }
+
+        public void Add(Figure figure)
+        {
+            figures.Add(figure);
+        }
+
+        public Figure GetValue(int index)
+        {
+            return figures[index];
+        }
+        
+        public int Count
+        {
+            get
+            {
+                return figures.Count;
+            }
+        }
+
+        #region Q1
+        public Figure this[int index]
+        {
+            get
+            {
+                return GetValue(index);
+            }
+        }
+        #endregion
+
+        #region Q2
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return figures.GetEnumerator();
+        }
+
+        public IEnumerator<Figure> GetEnumerator()
+        {
+            return ((IEnumerable<Figure>)figures).GetEnumerator();
+        }
+        #endregion
+    }
+}
---
+++ FiguresContainer/FiguresContainer/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FiguresContainer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FiguresContainer container = new FiguresContainer();
+
+            for (int i = 0; i < 10; ++i)
+            {
+                Figure f = new Figure();
+                f.counter = i;
+                container.Add(f);
+            }
+
+            string firstSum = "";
+            for (int i = 0; i < container.Count; ++i)
+            {
+                Figure f = container[i];
+                firstSum += f.counter.ToString() + ", ";
+            }
+
+            string secondSum = "";
+            foreach (Figure f in container)
+            {
+                secondSum += f.counter.ToString() + ", ";
+            }
+
+            textBox1.Text = firstSum;
+            textBox2.Text = secondSum;
+        }
+    }
+}
